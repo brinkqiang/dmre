e780076b2630764fec6a8d96a5681d23f426a567
brinkqiang <brink.qiang@gmail.com>
2024-07-25 17:13:48 +0800

    add

--- test/dmretest/dmretest.cpp
+++ test/dmretest/dmretest.cpp
@@ -1,31 +1,42 @@
 #include "dmre.h"
 #include <iostream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 static constexpr auto pattern = ctll::fixed_string
 {
-	 R"(^\s*#include\s*((?:<)(?<PATH>.*?)(?:>)|(?:")(?<PATH>.*?)(?:")))"
+	R"(^\s*#include\s*((?:<)(?<PATH>.*?)(?:>)|(?:")(?<PATH>.*?)(?:")))"
 };
 
 constexpr auto match(std::string_view sv) noexcept
 {
-    return ctre::match<pattern>(sv);
+	return ctre::match<pattern>(sv);
+}
+
+std::vector<std::string> split_string(const std::string& str) {
+	std::vector<std::string> result;
+	std::istringstream iss(str);
+	for (std::string line; std::getline(iss, line); ) {
+		result.push_back(line);
+	}
+	return result;
 }
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::string> includes = {
-		"#include \"dmos.h\"",
-		"#include <atomic>",
-		"#include <mutex>",
-		"#include <mach-o/dyld.h>",
-		"#include \"mach-o/dyld.h\"",
-		"   #include <iostream>  ", // 添加一些额外的空格
-		"#include \"path/to/file.h\"",
-		"int main() { }" // 非 #include 行
-	};
+	std::string includes = R"(#include "dmos.h"
+#include <atomic>
+#include <mutex>
+#include <mach-o/dyld.h>
+#include "mach-o/dyld.h"
+   #include <iostream>  
+#include "path/to/file.h"
+int main() { })";
 
-	for (std::string_view line : includes) {
+	std::vector<std::string> include_lines = split_string(includes);
+
+	for (const std::string& line : include_lines) {
 		auto f = ctre::search<pattern>(line);
 		if (f) {
 			// Correctly use the id for the capture group
@@ -35,6 +46,5 @@ int main(int argc, char* argv[])
 			std::cout << "No match for: " << line << std::endl;
 		}
 	}
-
 	return 0;
 }
